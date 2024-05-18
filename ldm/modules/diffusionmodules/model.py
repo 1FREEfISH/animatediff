8b6e99fbdd3ec8b169558d603145a208e5b8eaf1
sachit3022 <sachit3022@gmail.com>
2024-05-18 19:43:05 -0400

    change type to attn_type, Fixing bug which doesnot have the variable type. Authors have failed to change it to type to attn_type. Curious if cross attn is not used in stable diffusion?

--- ldm/modules/diffusionmodules/model.py
+++ ldm/modules/diffusionmodules/model.py
@@ -288,7 +288,7 @@ def make_attn(in_channels, attn_type="vanilla", attn_kwargs=None):
     elif attn_type == "vanilla-xformers":
         print(f"building MemoryEfficientAttnBlock with {in_channels} in_channels...")
         return MemoryEfficientAttnBlock(in_channels)
-    elif type == "memory-efficient-cross-attn":
+    elif attn_type == "memory-efficient-cross-attn":
         attn_kwargs["query_dim"] = in_channels
         return MemoryEfficientCrossAttentionWrapper(**attn_kwargs)
     elif attn_type == "none":
